8c62d2420d85dc2095941b27c498bb91a2c74942
JuiPharm <jui_3065@hotmail.com>
2025-12-16 15:03:38 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  APP_NAME: "BHH ICU Stock Management",
+  TIMEZONE: "Asia/Bangkok",
+  LOCALE: "th-TH",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxk8YusmqCrn0fcPITsHYS_9UIYu9mdT-3R-pKjDyOy8R3TuLekUW0akCm0iWd_X_kcuA/exec", // https://script.google.com/macros/s/.../exec
+  LOGO_URL: "https://lh5.googleusercontent.com/d/1r7PM1ogHIbxskvcauVIYaQOfSHXWGncO",
+};
